18c66dcd6fef3eb641625874f5e24e30fe3a3305
Sohrab SF <sohrab.seyedfathi@gmail.com>
2018-07-02 14:42:38 -0700

    python 3.5

--- tune.sh
+++ tune.sh
@@ -4,37 +4,36 @@ PROJECT_NAME=$2
 
 lr="1e0"
 hidden="8"
-just create job single --project $USERNAME/$PROJECT_NAME --module mnist --learning_rate $lr --hidden1 $hidden --hidden2 $hidden --name lr-$lr-hidden-$hidden --instance-type c4.2xlarge --description "learning rate : $lr and number of hidden layer nodes =$hidden" --python-version 3 --time-limit 1h
+just create job single --project $USERNAME/$PROJECT_NAME --module mnist --learning_rate $lr --hidden1 $hidden --hidden2 $hidden --name lr-$lr-hidden-$hidden --instance-type c4.2xlarge --description "learning rate : $lr and number of hidden layer nodes =$hidden" --python-version 3.5 --time-limit 1h --commit d0a7e661ec5ed310ebcddc6eba08448e1a50eff0
 just start job -p $PROJECT_NAME/lr-$lr-hidden-$hidden
 echo "Job lr-$lr-hidden-$hidden started!"
 
 lr="1e-1"
 hidden="8"
-just create job single --project $USERNAME/$PROJECT_NAME --module mnist --learning_rate $lr --hidden1 $hidden --hidden2 $hidden --name lr-$lr-hidden-$hidden --instance-type c4.2xlarge --description "learning rate : $lr and number of hidden layer nodes =$hidden" --python-version 3 --time-limit 1h
+just create job single --project $USERNAME/$PROJECT_NAME --module mnist --learning_rate $lr --hidden1 $hidden --hidden2 $hidden --name lr-$lr-hidden-$hidden --instance-type c4.2xlarge --description "learning rate : $lr and number of hidden layer nodes =$hidden" --python-version 3.5 --time-limit 1h --commit d0a7e661ec5ed310ebcddc6eba08448e1a50eff0
 just start job -p $PROJECT_NAME/lr-$lr-hidden-$hidden
 echo "Job lr-$lr-hidden-$hidden started!"
 
 lr="1e-2"
 hidden="8"
-just create job single --project $USERNAME/$PROJECT_NAME --module mnist --learning_rate $lr --hidden1 $hidden --hidden2 $hidden --name lr-$lr-hidden-$hidden --instance-type c4.2xlarge --description "learning rate : $lr and number of hidden layer nodes =$hidden" --python-version 3 --time-limit 1h
+just create job single --project $USERNAME/$PROJECT_NAME --module mnist --learning_rate $lr --hidden1 $hidden --hidden2 $hidden --name lr-$lr-hidden-$hidden --instance-type c4.2xlarge --description "learning rate : $lr and number of hidden layer nodes =$hidden" --python-version 3.5 --time-limit 1h --commit d0a7e661ec5ed310ebcddc6eba08448e1a50eff0
 just start job -p $PROJECT_NAME/lr-$lr-hidden-$hidden
 echo "Job lr-$lr-hidden-$hidden started!"
 
 lr="1e0"
 hidden="64"
-just create job single --project $USERNAME/$PROJECT_NAME --module mnist --learning_rate $lr --hidden1 $hidden --hidden2 $hidden --name lr-$lr-hidden-$hidden --instance-type c4.2xlarge --description "learning rate : $lr and number of hidden layer nodes =$hidden" --python-version 3 --time-limit 1h
+just create job single --project $USERNAME/$PROJECT_NAME --module mnist --learning_rate $lr --hidden1 $hidden --hidden2 $hidden --name lr-$lr-hidden-$hidden --instance-type c4.2xlarge --description "learning rate : $lr and number of hidden layer nodes =$hidden" --python-version 3.5 --time-limit 1h --commit d0a7e661ec5ed310ebcddc6eba08448e1a50eff0
 just start job -p $PROJECT_NAME/lr-$lr-hidden-$hidden
 echo "Job lr-$lr-hidden-$hidden started!"
 
 lr="1e-1"
 hidden="64"
-just create job single --project $USERNAME/$PROJECT_NAME --module mnist --learning_rate $lr --hidden1 $hidden --hidden2 $hidden --name lr-$lr-hidden-$hidden --instance-type c4.2xlarge --description "learning rate : $lr and number of hidden layer nodes =$hidden" --python-version 3 --time-limit 1h
+just create job single --project $USERNAME/$PROJECT_NAME --module mnist --learning_rate $lr --hidden1 $hidden --hidden2 $hidden --name lr-$lr-hidden-$hidden --instance-type c4.2xlarge --description "learning rate : $lr and number of hidden layer nodes =$hidden" --python-version 3.5 --time-limit 1h --commit d0a7e661ec5ed310ebcddc6eba08448e1a50eff0
 just start job -p $PROJECT_NAME/lr-$lr-hidden-$hidden
 echo "Job lr-$lr-hidden-$hidden started!"
 
 lr="1e-2"
 hidden="64"
-just create job single --project $USERNAME/$PROJECT_NAME --module mnist --learning_rate $lr --hidden1 $hidden --hidden2 $hidden --name lr-$lr-hidden-$hidden --instance-type c4.2xlarge --description "learning rate : $lr and number of hidden layer nodes =$hidden" --python-version 3 --time-limit 1h
+just create job single --project $USERNAME/$PROJECT_NAME --module mnist --learning_rate $lr --hidden1 $hidden --hidden2 $hidden --name lr-$lr-hidden-$hidden --instance-type c4.2xlarge --description "learning rate : $lr and number of hidden layer nodes =$hidden" --python-version 3.5 --time-limit 1h --commit d0a7e661ec5ed310ebcddc6eba08448e1a50eff0
 just start job -p $PROJECT_NAME/lr-$lr-hidden-$hidden
 echo "Job lr-$lr-hidden-$hidden started!"
-
